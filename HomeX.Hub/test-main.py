0fe8b59395fb123cc06ba6e9f5405e34733287f2
Arthur Pimenta <arituca@gmail.com>
2018-12-05 21:15:25 -0200

    testing viability of digi xbee package

---
+++ HomeX.Hub/test-main.py
@@ -0,0 +1,58 @@
+import time
+
+from digi.xbee.models.status import NetworkDiscoveryStatus
+from digi.xbee.devices import XBeeDevice
+
+# TODO: Replace with the serial port where your local module is connected to.
+PORT = "/dev/tty.usbserial-A403B7KX"
+# TODO: Replace with the baud rate of your local module.
+BAUD_RATE = 9600
+
+def main():
+    print(" +---------------------------------------------+")
+    print(" | XBee Python Library Discover Devices Sample |")
+    print(" +---------------------------------------------+\n")
+
+    device = XBeeDevice(PORT, BAUD_RATE)
+
+    try:
+        device.open()
+
+        xbee_network = device.get_network()
+
+        xbee_network.set_discovery_timeout(15)  # 15 seconds.
+
+        xbee_network.clear()
+
+        # Callback for discovered devices.
+        def callback_device_discovered(remote):
+            print("Device discovered: %s" % remote)
+
+        # Callback for discovery finished.
+        def callback_discovery_finished(status):
+            if status == NetworkDiscoveryStatus.SUCCESS:
+                print("Discovery process finished successfully.")
+                # Get a list of the devices added to the network.
+                devices_list = xbee_network.get_devices()
+                print(devices_list)
+            else:
+                print("There was an error discovering devices: %s" % status.description)
+
+        xbee_network.add_device_discovered_callback(callback_device_discovered)
+
+        xbee_network.add_discovery_process_finished_callback(callback_discovery_finished)
+
+        xbee_network.start_discovery_process()
+
+        print("Discovering remote XBee devices...")
+
+        while xbee_network.is_discovery_running():
+            time.sleep(0.1)
+
+    finally:
+        if device is not None and device.is_open():
+            device.close()
+
+
+if __name__ == '__main__':
+    main()
